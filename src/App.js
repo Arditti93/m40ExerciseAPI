3d2b277f9a7bb1d5be560684c98e5262963281dd
Arditti93 <alex.arditti@hotmail.com>
2022-09-16 16:04:30 +0100

    display response from exercise api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css"
+//TODO import DisplayUsers componet here
 
-function App() {
+const API_URL = "https://wger.de/api/v2/exerciseinfo/"
+
+const App = () => {
+  const [exercise, setExercise]= useState()
+
+  useEffect (() =>{
+    searchExercises()
+  },[])
+
+  const searchExercises = async () => {
+    const req = await fetch(`${API_URL}`)
+    const res = await req.json()
+    // console.log(res.results)
+    setExercise(res.results)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      { exercise?.length > 0
+        ?(
+          <div className="container">
+            {exercise.map(({name, description, id}) => (
+              <div className="exercise">
+                <h1 key={id}>Name of exercise {name}</h1>
+                <p key={id}>{description}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No exercises found</h2>
+          </div>
+        )
+      }
     </div>
-  );
+  )
 }
 
 export default App;
+
